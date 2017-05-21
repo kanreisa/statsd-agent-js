9c4a52a02a3488c8b3003a60c3afce1edd84de48
Yuki KAN <re@pixely.jp>
2017-05-21 20:06:02 +0900

    testing

--- src/monitors/network-monitor.js
+++ src/monitors/network-monitor.js
@@ -13,6 +13,49 @@ const isPlatformLinux = process.platform === 'linux';
 
 const regexpIfaceRxTx = / ([a-z0-9]+): +([0-9]+) +([0-9]+) +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +([0-9]+) +([0-9]+)/;
 
+let lastRxBytes = -1;
+let lastRxPackets = -1;
+let lastTxBytes = -1;
+let lastTxPackets = -1;
+
+function getTraffics() {
+    
+    const netdev = fs.readFileSync("/proc/net/dev", {
+        encoding: "utf8",
+        flag: "r"
+    });
+
+    const [, rxBytes, rxPackets, txBytes, txPackets] = netdev.split("\n").slice(2, -1).map(line => {
+        const parts = line.match(regexpIfaceRxTx);
+        if (parts) {
+            return parts.slice(1);
+        } else {
+            return [""];
+        }
+    }).filter(line => line[0] === "eth0")[0];
+
+    if (lastRxBytes === -1) {
+        lastRxBytes = rxBytes;
+        lastRxPackets = rxPackets;
+        lastTxBytes = txBytes;
+        lastTxPackets = txPackets;
+    }
+
+    const ret = [
+        rxBytes - lastRxBytes,
+        rxPackets - lastRxPackets,
+        txBytes - lastTxBytes,
+        txPackets - lastTxPackets
+    ];
+
+    lastRxBytes = rxBytes;
+    lastRxPackets = rxPackets;
+    lastTxBytes = txBytes;
+    lastTxPackets = txPackets;
+
+    return ret;
+}
+
 class NetworkMonitor extends Monitor {
     constructor() {
         super('network');
@@ -32,20 +75,7 @@ class NetworkMonitor extends Monitor {
         socketStatisticsUtil
             .getTcpStateCounts()
             .then(tcpStateCounts => {
-                
-                const netdev = fs.readFileSync("/proc/net/dev", {
-                    encoding: "utf8",
-                    flag: "r"
-                });
-                const [, rxBytes, rxPackets, txBytes, txPackets] = netdev.split("\n").slice(2, -1).map(line => {
-                    const parts = line.match(regexpIfaceRxTx);
-                    if (parts) {
-                        return parts.slice(1);
-                    } else {
-                        return [""];
-                    }
-                }).filter(line => line[0] === "eth0")[0];
-
+                const [rxBytes, rxPackets, txBytes, txPackets] = getTraffics();
                 this.setStatistics([
                     ..._.pairs(tcpStateCounts),
                     ["eth0.rx.bytes", rxBytes],
