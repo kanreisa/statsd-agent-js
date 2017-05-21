152fcb5b9db34af78dc29866a8fef81ff54f6920
Yuki KAN <re@pixely.jp>
2017-05-21 19:56:21 +0900

    add bandwidth stats

--- src/monitors/network-monitor.js
+++ src/monitors/network-monitor.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const childProcess = require('child_process');
+const fs = require("fs");
 const _ = require('underscore');
 const netstat = require('node-netstat');
 const changeCase = require('change-case');
@@ -8,6 +10,8 @@ const socketStatisticsUtil = require('../utils/socket-statistics-util');
 const Monitor = require('./objects/monitor');
 
 const isPlatformLinux = process.platform === 'linux';
+
+const regexpIfaceRxTx = / ([a-z0-9]+): +([0-9]+) +([0-9]+) +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +([0-9]+) +([0-9]+)/;
 
 class NetworkMonitor extends Monitor {
     constructor() {
@@ -28,7 +32,27 @@ class NetworkMonitor extends Monitor {
         socketStatisticsUtil
             .getTcpStateCounts()
             .then(tcpStateCounts => {
-                this.setStatistics(_.pairs(tcpStateCounts));
+                
+                const netdev = fs.readFileSync("/proc/net/dev", {
+                    encoding: "utf8",
+                    flag: "r"
+                });
+                const [, rxBytes, rxPackets, txBytes, txPackets] = netdev.split("\n").slice(2, -1).map(line => {
+                    const parts = line.match(regexpIfaceRxTx);
+                    if (parts) {
+                        return parts.slice(1);
+                    } else {
+                        return [""];
+                    }
+                }).filter(line => line[0] === "eth0")[0];
+
+                this.setStatistics([
+                    ..._.pairs(tcpStateCounts),
+                    ["eth0.rx.bytes", rxBytes],
+                    ["eth0.rx.packets", rxPackets],
+                    ["eth0.tx.bytes", txBytes],
+                    ["eth0.tx.packets", txPackets]
+                ]);
             })
             .catchConsoleError();
     }
